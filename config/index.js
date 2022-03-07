c418f80dfff0bf57785c712a8a23ad9705df4800
Vadim Bowbel <vadim.bowbel@outlook.de>
2022-03-07 01:16:48 +0100

    changed everything to dark theme

--- config/index.js
+++ config/index.js
@@ -15,14 +15,14 @@ module.exports = {
   // -- THEME SETTINGS -----
   colors: {
     lightTheme: {
-      primary: "#000000",
-      secondary: "#FFF4D9",
-      tertiary: "#F2F2F2",
-      text: "#000000",
-      subtext: "#555555",
-      background: "#FFFFFF",
-      card: "#FFFFFF",
-      scrollBar: "rgba(0, 0, 0, 0.5)",
+      primary: "#FAFAFA",
+      secondary: "#2A2926",
+      tertiary: "#252525",
+      text: "rgba(255, 255, 255, 0.87)",
+      subtext: "#AAAAAA",
+      background: "#202023",
+      card: "#202023",
+      scrollBar: "rgba(255, 255, 255, 0.5)",
       boxShadow: "rgba(0, 0, 0, 0.16)",
       boxShadowHover: "rgba(0, 0, 0, 0.32)",
     },
